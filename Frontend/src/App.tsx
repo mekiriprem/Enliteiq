b23a0867cd91275f32e42af40facec6f88e1fa20
mekiriprem <premchandu.m@zynlogic.com>
2025-06-12 19:04:29 +0530

    done

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -35,7 +35,7 @@ import AdminDashboard from "./pages/AdminDashboard";
 import ExamResultsPage from "./components/Dashbordspages/examresults";
 import Login from "./pages/Login";
 import SkillDevelopmentSection from "./pages/SkillDevelopment";
-import Aipage from "./pages/AIpage";
+import Aipage from "./pages/AIPage.tsx";
 import Forgotpassword from "./pages/ForgotPassword";
 import SalesTeam from "./components/Dashbordspages/Salesteam";
 import Schools from "./components/Dashbordspages/Schools";
